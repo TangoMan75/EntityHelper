23b8f1a829505a80ef7de072d8beff14b1e91314
MORIN Matthias <matthias.morin@gmail.com>
2017-11-08 17:58:57 +0100

    __isset()

--- Traits/HasRelationships.php
+++ Traits/HasRelationships.php
@@ -8,49 +8,34 @@ use Doctrine\Common\Collections\Collection;
 /**
  * Trait HasRelationships
  * ======================
- * 
  * This trait provide magic methods to handle both OWNING and INVERSE side of bidirectional relationships.
- * 
  *  - Both entities MUST use `HasRelationships` trait.
  *  - Both entities MUST define properties with appropriate annotations.
  *  - `cascade={"persist"}` annotation is MANDATORY (will allow bidirectional linking between entities).
- * 
  * OneToOne relationships
  * ----------------------
- * 
  * - `cascade={"remove"}` will avoid orphan `Item` on `Owner` deletion (optional).
- * 
  * ```php
  * use Doctrine\ORM\Mapping as ORM;
- * 
  * // ...
- * 
  * /**
  *  * {@}var Item
  *  * {@}ORM\OneToOne(targetEntity="AppBundle\Entity\Item", inversedBy="owner", cascade={"persist", "remove"})
  *  {@*}
  *  private $item;
  * ```
- * 
  * ManyToMany relationships
  * ------------------------
- * 
  * ### Entity
- * 
  * #### Properties
- * 
  * - property must own `{@}var ArrayCollection`
  * - `{@}ORM\OrderBy({"id"="DESC"})` will allow to define custom orderBy when fetching `items` (optional).
- * 
  * `src\AppBundle\Entity\Item.php`
  * ```php
  * namespace AppBundle\Entity;
- * 
  * use Doctrine\Common\Collections\ArrayCollection;
  * use Doctrine\ORM\Mapping as ORM;
- * 
  * // ...
- * 
  * /**
  *  * {@}var ArrayCollection
  *  * {@}ORM\ManyToMany(targetEntity="AppBundle\Entity\Item", inversedBy="owners", cascade={"persist"})
@@ -58,11 +43,8 @@ use Doctrine\Common\Collections\Collection;
  *  {@*}
  * private $items = [];
  * ```
- * 
  * #### Constructor
- * 
  * Constructor must initialize mapped property with `ArrayCollection`
- * 
  * `src\AppBundle\Entity\Item.php`
  * ```php
  * /**
@@ -73,11 +55,8 @@ use Doctrine\Common\Collections\Collection;
  *     $this->Items = new ArrayCollection();
  * }
  * ```
- * 
  * ### FormTypes
- * 
  * Requires formType to own `'by_reference' => false,` attribute to force use of `add` and `remove` methods.
- * 
  * `src\AppBundle\Form\ItemType.php`
  * ```php
  * /**
@@ -113,10 +92,24 @@ use Doctrine\Common\Collections\Collection;
 trait HasRelationships
 {
     /**
-     * @param $method
+     * @param $name
+     *
+     * @return bool
+     */
+    public function __isset($name)
+    {
+        if (property_exists($this, $name)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @param $methodFullName
      * @param $arguments
      *
-     * @return null
+     * @return mixed|null
      */
     public function __call($methodFullName, $arguments)
     {
@@ -133,54 +126,54 @@ trait HasRelationships
         if (is_array($this->$property) || $this->$property instanceof Collection) {
             switch ($method) {
                 case 'set':
-                return $this->setMany($property, $arguments[0]);
-                break;
+                    return $this->setMany($property, $arguments[0]);
+                    break;
                 case 'get':
-                return $this->get($property);
-                break;
+                    return $this->get($property);
+                    break;
                 case 'has':
-                return $this->has($property, $arguments[0]);
-                break;
+                    return $this->has($property, $arguments[0]);
+                    break;
                 case 'add':
-                return $this->add($property, $arguments[0]);
-                break;
+                    return $this->add($property, $arguments[0]);
+                    break;
                 case 'remove':
-                return $this->remove($property, $arguments[0]);
-                break;
+                    return $this->remove($property, $arguments[0]);
+                    break;
                 case 'link':
-                $this->linkMany($property, $arguments[0]);
-                break;
+                    $this->linkMany($property, $arguments[0]);
+                    break;
                 case 'unLink':
-                $this->unLinkMany($property, $arguments[0]);
-                break;
+                    $this->unLinkMany($property, $arguments[0]);
+                    break;
                 case '': // direct access like in twig
-                return $this->get($property);
-                break;
+                    return $this->get($property);
+                    break;
             }
         } else {
             switch ($method) {
                 case 'set':
-                return $this->set($property, $arguments[0]);
-                break;
+                    return $this->set($property, $arguments[0]);
+                    break;
                 case 'get':
-                return $this->get($property);
-                break;
+                    return $this->get($property);
+                    break;
                 case 'is':
-                if (is_bool($this->$property)) {
-                    return $this->is($property);
-                } else {
-                    throw new \BadMethodCallException('Method '.$methodFullName.' doesn\'t exist');
-                }
-                break;
+                    if (is_bool($this->$property)) {
+                        return $this->is($property);
+                    } else {
+                        throw new \BadMethodCallException('Method '.$methodFullName.' doesn\'t exist');
+                    }
+                    break;
                 case 'link':
-                $this->link($property, $arguments[0]);
-                break;
+                    $this->link($property, $arguments[0]);
+                    break;
                 case 'unLink':
-                $this->unLink($property, $arguments[0]);
-                break;
+                    $this->unLink($property, $arguments[0]);
+                    break;
                 case '': // direct access like in twig
-                return $this->get($property);
-                break;
+                    return $this->get($property);
+                    break;
             }
         }
 
