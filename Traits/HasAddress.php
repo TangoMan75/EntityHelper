a183ede3a3f3289751eb4ebddec00b0278accb52
MORIN Matthias <matthias.morin@gmail.com>
2017-11-17 18:37:33 +0100

    HasAddress

--- Traits/HasAddress.php
+++ Traits/HasAddress.php
@@ -7,8 +7,8 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * Trait HasAddress
- *
  * Requires entity to be marked with "HasLifecycleCallbacks" annotation.
+ *
  * @package TangoMan\EntityHelper
  */
 Trait HasAddress
@@ -60,6 +60,26 @@ Trait HasAddress
      * @ORM\Column(type="string", nullable=true)
      */
     protected $country;
+
+    /**
+     * @var float
+     * @Assert\Type(
+     *     type="digit",
+     *     message="La latitude ne peut contenir que des caractères numériques."
+     * )
+     * @ORM\Column(name="lat", type="string", length=255, nullable=true)
+     */
+    protected $lat;
+
+    /**
+     * @var float
+     * @Assert\Type(
+     *     type="digit",
+     *     message="La longitude ne peut contenir que des caractères numériques."
+     * )
+     * @ORM\Column(name="lng", type="string", length=255, nullable=true)
+     */
+    protected $lng;
 
     /**
      * @return string
@@ -179,5 +199,74 @@ Trait HasAddress
         $this->city = $city;
 
         return $this;
+    }
+
+    /**
+     * @param float $lat
+     *
+     * @return $this
+     */
+    public function setLat($lat)
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLat()
+    {
+        return $this->lat;
+    }
+
+    /**
+     * @param float $lng
+     *
+     * @return $this
+     */
+    public function setLng($lng)
+    {
+        $this->lng = $lng;
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLng()
+    {
+        return $this->lng;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullAddress()
+    {
+        $address = [];
+        if ($this->getStreet()) {
+            $address[] = $this->getStreet();
+        }
+
+        if ($this->getStreet2()) {
+            $address[] = $this->getStreet2();
+        }
+
+        if ($this->getZipCode()) {
+            $address[] = $this->getZipCode();
+        }
+
+        if ($this->getCity()) {
+            $address[] = $this->getCity();
+        }
+
+        if ($this->getCountry()) {
+            $address[] = $this->getCountry();
+        }
+
+        return implode(', ', $address);
     }
 }
